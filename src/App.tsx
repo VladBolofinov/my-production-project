d68a8ef440b23aa81c356c1a7c1a34275d6b4882
Vlad Bolofinov <vlad.bolofinov@gmail.com>
2023-09-08 12:39:55 +0300

    added classes function

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import {Link, Route, Routes} from "react-router-dom";
 import Component from "./component/Component";
-import './index.scss';
+import './styles/index.scss';
 import {AboutPageAsync} from "./pages/AboutPage/AboutPageAsync";
 import {MainPageAsync} from "./pages/MainPage/MainPageAsync";
-import {Suspense} from "react";
-const App = () => {
-    return (
-        <div className='app'>
-            asdasdasdwad
+import {Suspense, useContext, useState} from "react";
+import {Theme, ThemeContext} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
+import {classNames} from "./styles/helpers/classNames";
 
+const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+        <div className={classNames('app',{hovered:true, selected: false,}, [theme,'cls2','cls3'])}>
+            <button onClick={toggleTheme}>change theme</button>
             <Link to={'/main'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
